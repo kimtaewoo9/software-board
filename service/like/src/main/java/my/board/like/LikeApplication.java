93217c51749b8b58d726c8f56926b4c3f16818c7
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-05-26 14:41:44 +0900

    feat: initial project setup

---
+++ service/like/src/main/java/my/board/like/LikeApplication.java
@@ -0,0 +1,13 @@
+package my.board.like;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class LikeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LikeApplication.class, args);
+    }
+}
