fded15facde2289638ac752e99ead45e5b46127b
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-05-29 16:18:34 +0900

    refactor: 주석 삭제

--- service/article/src/main/java/my/board/article/service/ArticleService.java
+++ service/article/src/main/java/my/board/article/service/ArticleService.java
@@ -17,74 +17,73 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ArticleService {
 
-    private final Snowflake snowflake = new Snowflake();
-    private final ArticleRepository articleRepository;
+	private final Snowflake snowflake = new Snowflake();
+	private final ArticleRepository articleRepository;
 
-    @Transactional
-    public ArticleResponse create(ArticleCreateRequest articleCreateRequest) {
-        Article newArticle = Article.create(
-            snowflake.nextId(),
-            articleCreateRequest.getTitle(),
-            articleCreateRequest.getContent(),
-            articleCreateRequest.getBoardId(),
-            articleCreateRequest.getWriterId()
-        );
-        Article savedArticle = articleRepository.save(newArticle);
+	@Transactional
+	public ArticleResponse create(ArticleCreateRequest articleCreateRequest) {
+		Article newArticle = Article.create(
+			snowflake.nextId(),
+			articleCreateRequest.getTitle(),
+			articleCreateRequest.getContent(),
+			articleCreateRequest.getBoardId(),
+			articleCreateRequest.getWriterId()
+		);
+		Article savedArticle = articleRepository.save(newArticle);
 
-        return ArticleResponse.from(savedArticle);
-    }
+		return ArticleResponse.from(savedArticle);
+	}
 
-    @Transactional
-    public ArticleResponse update(Long articleId, ArticleUpdateRequest articleUpdateRequest) {
-        Article article = articleRepository.findById(articleId).orElseThrow(
-            () -> new EntityNotFoundException("article not found")
-        );
+	@Transactional
+	public ArticleResponse update(Long articleId, ArticleUpdateRequest articleUpdateRequest) {
+		Article article = articleRepository.findById(articleId).orElseThrow(
+			() -> new EntityNotFoundException("article not found")
+		);
 
-        String title = articleUpdateRequest.getTitle();
-        String content = articleUpdateRequest.getContent();
-        article.update(title, content);
+		String title = articleUpdateRequest.getTitle();
+		String content = articleUpdateRequest.getContent();
+		article.update(title, content);
 
-        return ArticleResponse.from(article);
-    }
+		return ArticleResponse.from(article);
+	}
 
-    public ArticleResponse read(Long articleId) {
-        Article article = articleRepository.findById(articleId).orElseThrow(
-            () -> new EntityNotFoundException("article not found")
-        );
+	public ArticleResponse read(Long articleId) {
+		Article article = articleRepository.findById(articleId).orElseThrow(
+			() -> new EntityNotFoundException("article not found")
+		);
 
-        return ArticleResponse.from(article);
-    }
+		return ArticleResponse.from(article);
+	}
 
-    @Transactional
-    public void delete(Long articleId) {
-        articleRepository.deleteById(articleId);
-    }
+	@Transactional
+	public void delete(Long articleId) {
+		articleRepository.deleteById(articleId);
+	}
 
-    public ArticlePageResponse readAll(Long boardId, Long page, Long pageSize) {
-        // offset은 page와 pageSize로 계산해서 articleRepository로 넘긴다 .
-        List<ArticleResponse> articleResponse = articleRepository.findAll(boardId,
-                (page - 1) * pageSize,
-                pageSize)
-            .stream()
-            .map(ArticleResponse::from)
-            .toList();
+	public ArticlePageResponse readAll(Long boardId, Long page, Long pageSize) {
+		List<ArticleResponse> articleResponse = articleRepository.findAll(boardId,
+				(page - 1) * pageSize,
+				pageSize)
+			.stream()
+			.map(ArticleResponse::from)
+			.toList();
 
-        Long articleCount = articleRepository.count(
-            boardId,
-            PageLimitCalculator.calculatePageLimit(page, pageSize, 10L)
-        );
+		Long articleCount = articleRepository.count(
+			boardId,
+			PageLimitCalculator.calculatePageLimit(page, pageSize, 10L)
+		);
 
-        return ArticlePageResponse.of(articleResponse, articleCount);
-    }
+		return ArticlePageResponse.of(articleResponse, articleCount);
+	}
 
-    public List<ArticleResponse> readAllInfiniteScroll(Long boardId, Long pageSize,
-        Long lastArticleId) {
-        List<Article> articles = lastArticleId == null ?
-            articleRepository.findAllInfiniteScroll(boardId, pageSize) :
-            articleRepository.findAllInfiniteScroll(boardId, pageSize, lastArticleId);
+	public List<ArticleResponse> readAllInfiniteScroll(Long boardId, Long pageSize,
+		Long lastArticleId) {
+		List<Article> articles = lastArticleId == null ?
+			articleRepository.findAllInfiniteScroll(boardId, pageSize) :
+			articleRepository.findAllInfiniteScroll(boardId, pageSize, lastArticleId);
 
-        return articles.stream()
-            .map(ArticleResponse::from)
-            .toList();
-    }
+		return articles.stream()
+			.map(ArticleResponse::from)
+			.toList();
+	}
 }
