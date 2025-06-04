8b5f78cee58c4c8432cc6446c1861b782fad23df
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-06-04 13:28:37 +0900

    🚀feat: outbox 모듈 구현 initialize

--- common/outbox-message-relay/src/main/java/my/board/common/outboxmessagerelay/MessageRelayConstants.java
+++
@@ -1,9 +0,0 @@
-package my.board.common.outboxmessagerelay;
-
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class MessageRelayConstants {
-    public static final int SHARD_COUNT = 4; // 임의의 값
-}
