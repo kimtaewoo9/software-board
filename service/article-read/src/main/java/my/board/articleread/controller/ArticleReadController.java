10eb4aae4b935b5af8b5af4346b3e1e9f31bc0d3
kitmtaewoo <bezzi1654@kookmin.ac.kr>
2025-06-08 23:20:50 +0900

    feat: 게시글 목록 최적화 - controller 구현

--- service/article-read/src/main/java/my/board/articleread/controller/ArticleReadController.java
+++ service/article-read/src/main/java/my/board/articleread/controller/ArticleReadController.java
@@ -1,10 +1,13 @@
 package my.board.articleread.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import my.board.articleread.service.ArticleReadService;
+import my.board.articleread.service.response.ArticleReadPageResponse;
 import my.board.articleread.service.response.ArticleReadResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +20,23 @@ public class ArticleReadController {
 	public ArticleReadResponse read(
 		@PathVariable("articleId") Long articleId) {
 		return articleReadService.read(articleId);
+	}
+
+	@GetMapping("/v1/articles")
+	public ArticleReadPageResponse readAll(
+		@RequestParam("boardId") Long boardId,
+		@RequestParam("page") Long page,
+		@RequestParam("pageSize") Long pageSize
+	) {
+		return articleReadService.readAll(boardId, page, pageSize);
+	}
+
+	@GetMapping("/v1/articles/infinite-scroll")
+	public List<ArticleReadResponse> readAllInfiniteScroll(
+		@RequestParam("boardId") Long boardId,
+		@RequestParam(value = "lastArticleId", required = false) Long lastArticleId,
+		@RequestParam("pageSize") Long pageSize
+	) {
+		return articleReadService.readAllInfiniteScroll(boardId, lastArticleId, pageSize);
 	}
 }
